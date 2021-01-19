1105a86ede2cc66357341e5a5a9e083c528d1e06
Pierre Rogier <pierre.rogier3@gmail.com>
2021-01-19 19:51:21 +0100

    setup filtering functionnality

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,9 +11,16 @@ import {
 
 const filter_reducer = (state, action) => {
     switch (action.type) {
-
         case LOAD_PRODUCTS:
-            return { ...state, all_products: [...action.payload], filtered_products: [...action.payload] };
+            let maxPrice = action.payload.map((product) => product.price);
+            maxPrice = Math.max(...maxPrice);
+
+            return {
+                ...state,
+                all_products: [...action.payload],
+                filtered_products: [...action.payload],
+                filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
+            };
 
         case SET_LISTVIEW:
             return { ...state, grid_view: false };
@@ -42,6 +49,56 @@ const filter_reducer = (state, action) => {
             }
 
             return { ...state, filtered_products: tempProducts };
+
+        case UPDATE_FILTERS:
+            const { name, value } = action.payload;
+            return { ...state, filters: { ...state.filters, [name]: value } };
+
+        case FILTER_PRODUCTS:
+            const { all_products } = state;
+            const { text, category, company, color, price, shipping } = state.filters;
+            let newProducts = [...all_products];
+
+            // search
+            if (text) {
+                newProducts = newProducts.filter((product) => product.name.toLowerCase().includes(text));
+            }
+            // category
+            if (category !== "all") {
+                newProducts = newProducts.filter((product) => product.category === category);
+            }
+            // company
+            if (company !== "all") {
+                newProducts = newProducts.filter((product) => product.company === company);
+            }
+            // colors
+            if (color !== "all") {
+                newProducts = newProducts.filter((product) => {
+                    return product.colors.find((c) => c === color);
+                });
+            }
+            // price
+            newProducts = newProducts.filter((product) => product.price <= price);
+            // shipping
+            if (shipping) {
+                newProducts = newProducts.filter((product) => product.shipping === true);
+            }
+
+            return { ...state, filtered_products: newProducts };
+
+        case CLEAR_FILTERS:
+            return {
+                ...state,
+                filters: {
+                    ...state.filters,
+                    text: "",
+                    company: "all",
+                    category: "all",
+                    color: "all",
+                    price: state.filters.max_price,
+                    shipping: false,
+                },
+            };
 
         default:
             return state;
